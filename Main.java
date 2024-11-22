0d8fdb4a6a8b4eab539a5135253d132fbf9babab
allllleeeex <agepp27@gmail.com>
2024-11-22 14:05:05 +0000

    hhhhhh

--- Main.java
+++ Main.java
@@ -2,11 +2,7 @@ public class Main {
 public static void main(String[] args) {
 {
     boolean[][] schedule = new boolean [8][60];
-    AppointmentBook a = new AppointmentBook(schedule)
+    AppointmentBook a = new AppointmentBook(schedule);
     System.out.println(a);
-
-    boolean[][] schedule = new boolean [10][14];
-    AppointmentBook b = new AppointmentBook(schedule);
-    System.out.println(b);
 
 } } }
