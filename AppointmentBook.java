d1c1b30eac56366ae131c7d6e0f0afb4cc0c8426
allllleeeex <agepp27@gmail.com>
2024-12-19 14:05:39 +0000

    gggg

--- AppointmentBook.java
+++ AppointmentBook.java
@@ -14,15 +14,37 @@ private boolean isMinuteFree(int period, int minute)
      
 private void reserveBlock(int period, int startMinute, int duration) 
 {
+    for(int i = startMinute; i < startMinute + duration; i++) {
+        schedule[period - 1][i] = false;
+    }
 }
        
-public int findFreeBlock(int period, int duration) 
-{
+public int findFreeBlock(int period, int duration) {
     int block = 0;
+    for(int i = 0; i < 60; i++) {
+        if (isMinuteFree(period, i)){
+            block ++;
+            if(block == duration) {
+                return i - duration + 1;
+            }
+        }
+        else {
+            block = 0;
+        }
+    }
+    return -1;
 }
         
 public boolean makeAppointment(int startPeriod, int endPeriod, int duration)
 {
+    for(int i = startPeriod; i <= endPeriod; i++){
+        int freeBlock = findFreeBlock(i, duration);
+        if (freeBlock > -1 )
+        {
+            reserveBlock(i, freeBlock, duration);
+            return true;
+        }
+    }
     return false;
 }
 public void printPeriod (int period)
